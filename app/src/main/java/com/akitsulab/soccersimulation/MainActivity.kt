643e2dbe82f696f72fc279849ae8a48fd40de9c4
長岡聡太 <>
2024-01-03 14:58:21 +0900

    ✨ ユーザー一覧

--- app/src/main/java/com/akitsulab/soccersimulation/MainActivity.kt
+++ app/src/main/java/com/akitsulab/soccersimulation/MainActivity.kt
@@ -3,13 +3,23 @@ package com.akitsulab.soccersimulation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.akitsulab.soccersimulation.ui.theme.SoccersimulationTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +32,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MainContent()
                 }
             }
         }
@@ -30,17 +40,62 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainContent(modifier: Modifier = Modifier) {
+    Column(
+        horizontalAlignment = Alignment.Start,
+        modifier = Modifier
+            .padding(10.dp)
+            .verticalScroll(rememberScrollState())
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.icon_user),
+                contentDescription = "ユーザーアイコン",
+                modifier = Modifier
+                    .size(100.dp)
+            )
+            Text(
+                text = "織田信長",
+                modifier = modifier
+            )
+        }
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.icon_user),
+                contentDescription = "ユーザーアイコン",
+                modifier = Modifier
+                    .size(100.dp)
+            )
+            Text(
+                text = "豊臣秀吉",
+                modifier = modifier
+            )
+        }
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.icon_user),
+                contentDescription = "ユーザーアイコン",
+                modifier = Modifier
+                    .size(100.dp)
+            )
+            Text(
+                text = "徳川家康",
+                modifier = modifier
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     SoccersimulationTheme {
-        Greeting("Android")
+        MainContent()
     }
 }
